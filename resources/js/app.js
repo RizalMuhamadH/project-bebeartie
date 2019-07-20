76969125b3b6467d4c40c287cff93edbfd7aafc9
Rizal Muhamad H <rizalmuhamadh@gmail.com>
2019-07-20 14:10:40 +0700

    add component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+import VueRouter from 'vue-router';
+
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -22,11 +24,26 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
+ * Vue Router
+ *
+ * @link http://router.vuejs.org/en/installation.html
+ */
+Vue.use(VueRouter);
+
+// define routes for users
+const routes = [
+
+]
+
+const router = new VueRouter({ routes });
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
 const app = new Vue({
+    router,
     el: '#app',
 });
