5cebb4297009cbdab19b943eabfb645a0235e4b4
kubichura <rizal.muhamad@ayomedia.com>
2019-07-22 17:37:36 +0700

    Create CategoryController.php

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::latest()->paginate(10);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = request()->validate([
+            'name' => 'required',
+            'desc' => 'required'
+        ]);
+
+        Category::create([
+            'name' => $data['name'],
+            'desc' => $data['desc']
+        ]);
+
+        return ['message' => 'Success'];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        $category = Category::findOrFail($category);
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = request()->validate([
+            'name' => 'required',
+            'desc' => 'required'
+        ]);
+
+        Category::where('id', $id)->update(array_merge($data));
+
+        return ['message' => 'Success'];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Category::findOrFail($id)->delete();
+
+        return ['message' => 'Success'];
+    }
+}
